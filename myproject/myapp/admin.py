ee12fed474e2a7c3c3c3c7bae9068498ce7ca4dc
Flammableobject <sodseein@gmail.com>
2022-04-21 21:30:36 +0700

    1.19.23 part1

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from myapp.models import Customer
+from myapp.models import Customer, Item, OrderItem, Invoice
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Customer, CustomerAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Item, ItemAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(OrderItem, OrderItemAdmin)
+
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Invoice, InvoiceAdmin)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -19,3 +19,24 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Customer.objects.create(user=instance)
         print('update_profile_signal: create a profile')
+
+#SKU (Single Keeping Unit)
+class Item(models.Model):
+    UNIT_NAME_CHOICE = [ {"ea","each"}, {"kg", "kilogram"}, {"g","gram"} ]
+    title = models.CharField(max_length=100)
+    unit = models.CharField(max_length=15, choices=UNIT_NAME_CHOICE, default="ea")
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2) #999.99
+    image=models.ImageField(upload_to="myimages") #from week5
+    description=models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return "%s"%(self.title)
+
+class OrderItem(models.Model):
+    customer = models.ForeignKey( Customer, on_delete=models.CASCADE)
+    item = models.ForeignKey( Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+class Invoice(models.Model):
+    customer = models.ForeignKey( Customer, on_delete=models.CASCADE)
+    order_item = models.ForeignKey( OrderItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
